8ca8faa56c2b88a429a25984ebfb68d202db7015
Dale Walter <dalewaltergh@gmail.com>
2022-09-26 22:36:12 +0800

    Profile Screen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -2,32 +2,13 @@ import { StyleSheet, FlatList, ScrollView } from "react-native";
 import { RootTabScreenProps } from "../types";
 import { View } from "../components/Themed";
 
+import MasonryList from "../components/MasonryList";
 import Pin from "../components/Pin";
+
 import pins from "../assets/data/pins";
 
-function HomeScreen({ navigation }: RootTabScreenProps<"Home">) {
-  return (
-    <ScrollView>
-      <View style={styles.container}>
-        {/* 1st column */}
-        <View style={styles.column}>
-          {pins
-            .filter((_, index) => index % 2 === 0)
-            .map((pin) => (
-              <Pin pin={pin} key={pin.id} />
-            ))}
-        </View>
-        {/* 2st column */}
-        <View style={styles.column}>
-          {pins
-            .filter((_, index) => index % 2 === 1)
-            .map((pin) => (
-              <Pin pin={pin} key={pin.id} />
-            ))}
-        </View>
-      </View>
-    </ScrollView>
-  );
+function HomeScreen() {
+  return <MasonryList pins={pins} />;
 }
 
 const styles = StyleSheet.create({
